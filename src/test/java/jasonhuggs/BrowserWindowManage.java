966e37ea639713a5efc8ff021c47605ad0012bbd
Farheen Sultana <Farheen Sultana@192.168.0.11>
2020-12-21 10:24:20 +0530

    updated browser manage program

--- src/test/java/jasonhuggs/BrowserWindowManage.java
+++ src/test/java/jasonhuggs/BrowserWindowManage.java
@@ -1,6 +1,8 @@
 package jasonhuggs;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -23,6 +25,19 @@ public class BrowserWindowManage
 		int w1=driver.manage().window().getSize().getWidth();
 		int h=driver.manage().window().getSize().getHeight();
 		System.out.println("Total width of browser window is " + w1 + "\nTotal height of browser window is " + h);
+		//Set size for browser window
+		Dimension d=new Dimension(200,100);
+		driver.manage().window().setSize(d);
+		Thread.sleep(5000);
+		//Get position details of browser window
+		int x=driver.manage().window().getPosition().getX();
+		int y=driver.manage().window().getPosition().getY();
+		System.out.println("X position of browser window is " + x + "\nY position of browser window is " + y);
+		//Set position of browser window
+		Point p=new Point(200,150);
+		driver.manage().window().setPosition(p);
+		Thread.sleep(5000);
+		//close site
+		driver.close();
 	}
-
 }
